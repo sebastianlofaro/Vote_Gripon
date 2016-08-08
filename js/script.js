a9f3701285832b75c78bea5da5a3dabd3cd1ae7b
sebastianlofaro <sebastianlofaro@me.com>
2016-08-08 17:32:13 -0500

    Layout Adjustments
    Fixed  an issue where content wrapper was not getting correct top
    margin value.

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ window.onscroll = function(e) {
   var blurValue = window.scrollY / 10;
   var blur = blurValue + "px";
   document.getElementById("video").style.webkitFilter = "blur(" + blur + ")";
+  document.getElementById("video").style.filter = "blur(" + blur + ")";
+
+  if (window.scrollY > 500) {
+    $("footer").css("display", "block");
+  }
+  else {
+    $("footer").css("display", "none");
+  }
 }
+
+
+var videoHeight = $("#video").css("height");
+
+$(document).ready(function() {
+  $("#wrapper").css("margin-top", videoHeight);
+  $("footer").css("display", "none");
+});
+
+$(window).on("resize", function() {
+  videoHeight = $("#video").css("height");
+  $("#wrapper").css("margin-top", videoHeight);
+
+})
